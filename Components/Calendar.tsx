6a290aca7274121398f7b0d28e29226d514b4a80
Giga Shavlishvili (200496) <giga.shavlishvili@ug.edu.ge>
2022-09-11 17:05:02 +0400

    app is done

--- Components/Calendar.tsx
+++ Components/Calendar.tsx
@@ -1,29 +1,40 @@
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Calendar.module.scss";
-import { leapYear,month_names_short,Weeks } from "../data/index";
-
+import { leapYear, month_names_short, Weeks } from "../data/index";
 
 var date = new Date();
-
-const Calendar = () => {
-    
-const [currentMonth, setCurrentMonth ] = useState<number>(date.getMonth())
-const [currentYears, setCurrentYears ] = useState<number>(date.getFullYear())
-const [crrentDay, setCurrentDay] = useState<number>(date.getDate())
-/* const [dayNumber, setDayNumber] = useState<any>(new Date(currentYears,currentMonth, 1 )) */
-const currDate = new Date(currentYears, currentMonth, 1);
-console.log(currDate.getDay());
-const dayNumber = currDate.getDay()
-  const monthSize = leapYear(currentYears)
-
-/*   const array = new Array(monthSize[currentMonth]).fill("day"); */
-let array: number[] = new Array(dayNumber).fill(null)
-for( let i = 0; i < monthSize[currentMonth]; i++ ){
- array.push(i)
+interface dataPickers {
+  setCurrentMonth: (value: number) => void;
+  setYears: (value: number) => void;
+  setCurrentDay: (value: number) => void;
+  setIsDrop: (value: boolean) => void;
+  currentDay: number;
+  years: number;
+  currentMonth: number;
 }
 
-console.log(crrentDay)
+const Calendar = ({
+  setCurrentMonth,
+  setYears,
+  setCurrentDay,
+  setIsDrop,
+  currentDay,
+  years,
+  currentMonth,
+}: dataPickers) => {
+  /* const [dayNumber, setDayNumber] = useState<any>(new Date(currentYears,currentMonth, 1 )) */
+  const currDate = new Date(years, currentMonth , 1);
+  console.log(currDate.getDay());
+  const dayNumber = currDate.getDay();
+  const monthSize = leapYear(years);
+
+  /*   const array = new Array(monthSize[currentMonth]).fill("day"); */
+  let array: number[] = new Array(dayNumber).fill(null);
+  for (let i = 0; i < monthSize[currentMonth - 1]; i++) {
+    array.push(i);
+  }
+
 
 
   return (
@@ -36,47 +47,55 @@ console.log(crrentDay)
             width={11}
             height={11}
             onClick={() => {
-                if(currentMonth === 0){
-                    setCurrentMonth(11)
-                    setCurrentYears(prev => prev - 1)
-                }else
-                setCurrentMonth(prev => prev - 1)
+              if (currentMonth === 1) {
+                setCurrentMonth(11);
+                setYears(years - 1);
+              } else setCurrentMonth(currentMonth - 1);
             }}
           />
-          <p>{month_names_short[currentMonth]}  {currentYears}</p>
+          <p>
+            {month_names_short[currentMonth - 1]}{" "}
+            {years}
+          </p>
           <Image
             src="/Vector2.png"
             alt="Picture of the author"
             width={11}
             height={11}
             onClick={() => {
-                if(currentMonth === 11){
-                    setCurrentMonth(0)
-                    setCurrentYears(prev => prev + 1)
-                }else
-                setCurrentMonth(prev => prev + 1)
+              if (currentMonth === 12) {
+                setCurrentMonth(1);
+                setYears(years + 1);
+              } else setCurrentMonth(currentMonth + 1);
             }}
           />
         </div>
-        <div className={styles.weeks}> 
-            {Weeks.map((el,i) =>{
-                return <span key={i}>{el}</span>
-            })}
+        <div className={styles.weeks}>
+          {Weeks.map((el, i) => {
+            return <span key={i}>{el}</span>;
+          })}
         </div>
-        <div className={styles.days}>{
-            
-            array.map((el,i) =>{
-                
-    if(el === null){
-   return <p key={i}></p>
-    }
-    else 
-    return <span style = {{border:`${el + 1 === crrentDay ? "1px solid #202945" : ""}`, borderRadius:"50%" }} onClick={() => {
-        setCurrentDay(el+1)
-/*         setIsDrop(false) */
-    }} key={i}>{el + 1}</span>
-                   })
-            }</div>
+        <div className={styles.days}>
+          {array.map((el, i) => {
+            if (el === null) {
+              return <p key={i}></p>;
+            } else
+              return (
+                <span
+                  onMouseEnter={(e) => {
+                    setCurrentDay(+e.currentTarget.innerHTML);
+                  }}
+                  onClick={() => {
+                    setCurrentDay(el + 1);
+                    setIsDrop(false);
+                  }}
+                  key={i}
+                >
+                  {el + 1}
+                </span>
+              );
+          })}
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,53 @@
-import  { useState } from "react"
+import  { useEffect, useState } from "react"
 import type { NextPage } from 'next'
 import Image from "next/image"
 import styles from '../styles/Home.module.scss'
 import Calendar from "../Components/Calendar"
-
+var date = new Date();
 const Home: NextPage = () => {
 const [isDrop, setIsDrop] = useState<boolean>(false)
+const [currentMonth, setCurrentMonth ] = useState(date.getMonth() + 1)
+const [years, setYears ] = useState(date.getFullYear())
+const [currentDay, setCurrentDay] = useState(0)
+
+
 
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container}>  
       <div className={styles.main}>
 
       <form action="/action_page.php">
         <div className={styles.datePicker}>
-          <div className={styles.datePicker__wrapper}>
+          <div className={styles.datePicker__wrapper} onClick={() =>{
+setIsDrop(true)
+      }}>
   <label htmlFor="date">from when available</label>
-  <input type="text" defaultValue="dd.mm.yyyy" id="date" name="date"/>
+
+  <div className={styles.inputs}>
+  <input type="text" id="date"  placeholder="dd" maxLength={2} name="date" value={  currentDay !== 0 ?  currentDay : ""} onChange={(e) =>{
+  currentDay < 30
+   setCurrentDay(+(e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1')))
+  }}/>.
+  <input type="text" id="date" placeholder="mm" maxLength={2} name="date" value={  currentDay !== 0 ? currentMonth : ""} onChange={(e) =>{
+   setCurrentMonth(+e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1'))
+
+   
+  }}/>.
+  <input type="text" id="date" placeholder="yyyy"  maxLength={4}  minLength={3} name="date" value={ currentDay !== 0 ? years : ""}   onChange={(e) =>{
+   setYears(+e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1'))
+  }}/>
   </div>
+  </div>
+  <label htmlFor="date">
   <Image
       src="/calendar.png"
       alt="Picture of the author"
       width={20}
       height={20}
-      onClick={() =>{
-setIsDrop(!isDrop)
-      }}
     />
-     {isDrop && <Calendar/>} 
+    </label>
+     {isDrop && <Calendar setCurrentMonth={setCurrentMonth} setYears={setYears}  setCurrentDay={setCurrentDay} currentMonth={currentMonth} years={years}  currentDay={currentDay} setIsDrop={setIsDrop}/>} 
   </div>
 </form>
 
